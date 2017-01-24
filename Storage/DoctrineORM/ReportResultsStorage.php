0392d0fda2e6ba8ddbee64024e2da960972d4b0d
Luis Galeas <galeaspablo@hotmail.com>
2017-01-24 12:00:03 +0000

    Optimize ReportResultsStorage Force X_To_One relationship to be loaded eagerly. Force X_To_Many relationship to be loaded lazily.

--- Storage/DoctrineORM/ReportResultsStorage.php
+++ Storage/DoctrineORM/ReportResultsStorage.php
@@ -3,6 +3,7 @@
 namespace FL\ReportsBundle\Storage\DoctrineORM;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\ClassMetadataInfo;
 use Doctrine\ORM\Query;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use FL\QBJSParser\Parsed\AbstractParsedRuleGroup;
@@ -17,10 +18,9 @@ class ReportResultsStorage implements ReportResultsStorageInterface
     protected $entityManager;
 
     /**
-     * @param EntityManagerInterface $entityManager
+     * @param EntityManagerInterface    $entityManager
      */
-    public function __construct(EntityManagerInterface $entityManager)
-    {
+    public function __construct(EntityManagerInterface $entityManager) {
         $this->entityManager = $entityManager;
     }
 
@@ -71,23 +71,33 @@ class ReportResultsStorage implements ReportResultsStorageInterface
             return [];
         }
 
-        // We are using resultIds because this means we don't have
-        // to hydrate the complete object graph and its associations.
-        // If those associations are OneToMany or ManyToMany,
-        // there would have been a serious performance degradation.
-        $qb = $this->entityManager->createQueryBuilder();
-        $qb->select('object')
+        /**
+         * We are doing this because this we should not be hydrating the complete
+         * object graph and its associations.
+         *
+         * Entity metadata is modified such that any __ToOne associations are
+         * eagerly loaded (no repeated queries to access values in each row).
+         *
+         * Entity metadata is modified such that any __ToMany associations are
+         * lazily loaded (prevents large memory consumption).
+         *
+         * @see http://ocramius.github.io/blog/doctrine-orm-optimization-hydration/
+         *
+         * Cannot use $queryBuilder->getResult(Query::HYDRATE_SIMPLEOBJECT) because
+         * it will not hydrate associations.
+         *
+         * If performance remains a problem, consider $queryBuilder->getResult(Query::HYDRATE_SCALAR).
+         * Note: More changes would be needed because $queryBuilder->getResult(Query::HYDRATE_SCALAR)
+         * does not return an array of hydrated objects.
+         */
+        $this->modifyMetadata($parsedRuleGroup->getClassName());
+        $unsortedObjects = $this->entityManager->createQueryBuilder()
+            ->select('object')
             ->from($parsedRuleGroup->getClassName(), 'object')
             ->where('object.id IN (:ids)')
-            ->setParameter('ids', $resultIds);
-
-        // @todo insert event for better hydration
-        // subscribers should only add Joins to this, not where/sort/group/etc.
-
-        // Query::HYDRATE_SIMPLEOBJECT does not hydrate OneToOne/ManyToOne associations.
-        // If performance of this method becomes a problem, consider
-        // adapting to ->getResult(Query::HYDRATE_SCALAR).
-        $unsortedObjects = $qb->getQuery()->getResult();
+            ->setParameter('ids', $resultIds)
+            ->getQuery()
+            ->getResult();
 
         $idsToObjects = [];
         foreach ($unsortedObjects as $object) {
@@ -115,5 +125,42 @@ class ReportResultsStorage implements ReportResultsStorageInterface
         $paginator = new Paginator($query, $fetchJoinCollection = true);
 
         return $paginator->count();
+    }
+
+    private $associationCache = [];
+
+    /**
+     * Forces X_To_One relationship to be loaded eagerly.
+     * Forces X_To_Many relationship to be loaded lazily.
+     *
+     * @param string $entityClass
+     */
+    private function modifyMetadata(string $entityClass)
+    {
+        $classMetadata = $this->entityManager->getClassMetadata($entityClass);
+        $associationMappings = $classMetadata->getAssociationMappings();
+        $associationCache = []; // used to prevent recursive execution
+
+        foreach($associationMappings as $mappingKey => $mapping ) {
+            $associationIdentifier = sprintf(
+                '%s::%s____%s',
+                $mapping['sourceEntity'],
+                $mapping['fieldName'],
+                $mapping['targetEntity']
+            );
+            if (in_array($associationIdentifier, $associationCache)) {
+                continue;
+            }
+            $this->associationCache[] = $associationIdentifier;
+
+            if (in_array($mapping['type'], [ClassMetadataInfo::MANY_TO_ONE, ClassMetadataInfo::ONE_TO_ONE, ClassMetadataInfo::TO_ONE])) {
+                $associationMappings[$mappingKey]['fetch'] = ClassMetadataInfo::FETCH_EAGER;
+            }
+            if (in_array($mapping['type'], [ClassMetadataInfo::ONE_TO_MANY, ClassMetadataInfo::MANY_TO_MANY])) {
+                $associationMappings[$mappingKey]['fetch'] = ClassMetadataInfo::FETCH_LAZY;
+            }
+        }
+
+        $this->entityManager->getClassMetadata($entityClass)->associationMappings = $associationMappings;
     }
 }
